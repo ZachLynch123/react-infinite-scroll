dab1cbd86764fc937c55800214228edf4e284003
Zach <Zachary.Lynch1337@gmail.com>
2019-02-18 16:40:01 -0800

    Infinite scroll works! Need to style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import UnSplash, { toJson } from 'unsplash-js';
 import InfiniteScroll from 'react-infinite-scroll-component';
+import Images from './components/images';
 import './App.css';
 
 class App extends Component {
@@ -15,31 +16,46 @@ class App extends Component {
   
 
   componentDidMount() {
-    
+    this.fetchRandomImages();
+  }
+
+  fetchRandomImages = () => {
     const unsplash = new UnSplash({
       applicationId: "76bb6285cf43bd300d2bf95da9dfa3658729c5770c211ba53756cc7e1493f594",
       secret: "7059170e8e0467f99b8a80951d30c21acba4bd8809317bcdabf05946aa4a93c7",
       callbackUrl: "http://localhost:3000/"
     });
 
-    unsplash.photos.getRandomPhoto({count: 12})
+    unsplash.photos.getRandomPhoto({count: 30})
     .then(toJson)
     .then(json => {
-      console.log(json);
-      
+      let arr = this.state.data;
+
+      for (var i = 0; i < json.length; i++) {
+        arr.push(json[i]);
+      }
+
+      this.setState({
+        data: arr
+      });
+
     })
   }
 
 
-
   render() {
+    const { data } = this.state;
     return (
       <div className="App">
         <InfiniteScroll
         dataLength={this.state.data.length}
+        next={this.fetchRandomImages}
+        hasMore={true}
         loader={<h4>loading..</h4>}
         >
-
+        {data.map( image => 
+          <Images key={image.id} img={image} />
+        )}
         </InfiniteScroll>
       </div>
     );
--- src/components/images.js
+++ src/components/images.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const Images = (props) => {
-
+    console.log(props.img);
+    return(
+        <img src={props.img.urls.thumb}/>
+    );
 }
 
 export default Images;
